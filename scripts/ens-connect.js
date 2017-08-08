96e813d606b77238c6dd1d68e9df3ef1f22d5b8b
Jack Tanner <ugmo04@users.noreply.github.com>
2017-08-08 16:30:22 +0100

    added ens utility scripts

---
+++ scripts/ens-connect.js
@@ -0,0 +1,9 @@
+var ENS = require('ethereum-ens');
+var Web3 = require('web3');
+
+// var web3 = new Web3();
+// var ens = new ENS(web3);
+
+// var fs = require('fs');
+// var web3 = require("web3");
+// var ensConnect = require("./ensutils-testnet.js");
