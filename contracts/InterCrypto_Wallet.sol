478b0ca372264c77774e5a628aa3292fc3b08d27
Jack Tanner <ugmo04@users.noreply.github.com>
2017-08-15 18:01:41 +0100

    indexed events

--- contracts/InterCrypto_Wallet.sol
+++ contracts/InterCrypto_Wallet.sol
@@ -5,55 +5,55 @@ import "github.com/ugmo04/inter-crypto/contracts/InterCrypto_Interface.sol";
 
 contract InterCrypto_Wallet is usingInterCrypto {
 
-    event Deposit();
-    event WithdrawalNormal(address withdrawal, uint amount);
-    event WithdrawalInterCrypto(uint transactionID);
+event Deposit(address indexed deposit, uint amount);
+event WithdrawalNormal(address indexed withdrawal, uint amount);
+event WithdrawalInterCrypto(uint indexed transactionID);
 
-    mapping (address => uint) public funds;
+mapping (address => uint) public funds;
 
-    function InterCrypto_Wallet() {}
+function InterCrypto_Wallet() {}
 
-    function () payable {}
+function () payable {}
 
-    function deposit() payable {
-      if (msg.value > 0) {
-          funds[msg.sender] += msg.value;
-          Deposit();
-      }
+function deposit() payable {
+  if (msg.value > 0) {
+      funds[msg.sender] += msg.value;
+      Deposit(msg.sender, msg.value);
+  }
+}
+
+function intercrypto_GetInterCryptoPrice() constant public returns (uint) {
+    return interCrypto.getInterCryptoPrice();
+}
+
+function withdrawalNormal() payable external {
+    uint amount = funds[msg.sender] + msg.value;
+    funds[msg.sender] = 0;
+    if(msg.sender.send(amount)) {
+        WithdrawalNormal(msg.sender, amount);
     }
-
-    function intercrypto_GetInterCryptoPrice() constant public returns (uint) {
-        return interCrypto.getInterCryptoPrice();
+    else {
+        funds[msg.sender] = amount;
     }
+}
 
-    function withdrawalNormal() payable external {
-        uint amount = funds[msg.sender] + msg.value;
-        funds[msg.sender] = 0;
-        if(msg.sender.send(amount)) {
-            WithdrawalNormal(msg.sender, amount);
-        }
-        else {
-            funds[msg.sender] = amount;
-        }
-    }
-
-    function withdrawalInterCrypto(string _coinSymbol, string _toAddress) external payable {
-        uint amount = funds[msg.sender];
-        funds[msg.sender] = 0;
-        uint transactionID = intercrypto_sendToOtherBlockchain(amount + msg.value, _coinSymbol, _toAddress);
-        WithdrawalInterCrypto(transactionID);
-    }
+function withdrawalInterCrypto(string _coinSymbol, string _toAddress) external payable {
+    uint amount = funds[msg.sender];
+    funds[msg.sender] = 0;
+    uint transactionID = intercrypto_sendToOtherBlockchain(amount + msg.value, _coinSymbol, _toAddress);
+    WithdrawalInterCrypto(transactionID);
+}
 
 
-    function intercrypto_Recover() onlyOwner external {
-        interCrypto.recover();
-    }
+function intercrypto_Recover() onlyOwner external {
+    interCrypto.recover();
+}
 
-    function intercrypto_Recoverable() onlyOwner public constant returns (uint) {
-        return interCrypto.recoverable(this);
-    }
+function intercrypto_Recoverable() public constant returns (uint) {
+    return interCrypto.recoverable(this);
+}
 
-    function kill() onlyOwner external {
-        selfdestruct(owner);
-    }
+function kill() onlyOwner external {
+    selfdestruct(owner);
+}
 }
